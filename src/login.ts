fe16855ae683bb654f6a8e5cbf380cf1172fe040
Anfsity <186920685+anfsity@users.noreply.github.com>
2026-07-10 22:44:11 +0800

    feat: add login

---
+++ src/login.ts
@@ -0,0 +1,42 @@
+import { createHash, randomBytes } from "crypto";
+import { Base64 } from "js-base64";
+import { stringify } from "qs";
+
+interface PKCEResult {
+  code_verifier: string;
+  code_challenge: string;
+}
+
+interface LoginInfo {
+  login_url: string;
+  code_verifier: string;
+}
+
+const LOGIN_URL = "https://app-api.pixiv.net/web/v1/login";
+const randToken = (len: number = 32) => randomBytes(len);
+const sha256 = (data: string | Buffer): Buffer =>
+  createHash("sha256").update(data).digest();
+
+const oauthPkce = (): PKCEResult => {
+  const code_verifier = Base64.fromUint8Array(randToken(), true);
+  const code_challenge = Base64.encodeURI(
+    Base64.fromUint8Array(sha256(code_verifier)),
+  );
+
+  return { code_verifier, code_challenge };
+};
+
+export default function generatePixivLogin(): LoginInfo {
+  const { code_verifier, code_challenge } = oauthPkce();
+
+  const params = {
+    code_challenge,
+    code_challenge_method: "S256",
+    client: "pixiv-android",
+  };
+
+  return {
+    login_url: `${LOGIN_URL}?${stringify(params)}`,
+    code_verifier,
+  };
+}
